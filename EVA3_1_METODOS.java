12c5aea17ef819aedb3e51f4da87bd528551fa6c
gaelnu32 <L24550335@chihuahua2.tecnm.mx>
2024-11-04 16:51:18 -0600

    Add files via upload

---
+++ EVA3_1_METODOS.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_1_metodos;
+
+import java.util.Scanner;
+/**
+ *
+ * @author lozan
+ */
+public class EVA3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+        Scanner captu = new Scanner(System.in);   
+// TODO code application logic here
+    // INVOCAR EL METODO, LLAMADA A METODO --> USARLO
+        ImprimirMensaje("HOLA MUNDO!!");
+    }
+    
+
+    //declaracion del metodo
+    //MODIFICADORES
+      //VALOR RETORNO
+        //NOMBRE
+                       //PARAMETROS (OPCIONALES)
+    
+public static void imprimirMensaje(String mensaje){
+     //Cuerpo del metodo
+    System.out.println("Tu mensaje" + mensaje);
+    //cuerpo del m
+}   
+
+
+
+}
